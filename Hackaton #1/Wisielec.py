aa5b76d85c1dbc03d8d82f9728eb082373bf2c40
Tomek <tomek.jedynak@onet.eu>
2022-05-28 13:14:59 +0200

    "hackaton update"

--- Hackaton #1/Wisielec.py
+++ Hackaton #1/Wisielec.py
@@ -1,12 +1,13 @@
 import random
 
-ZBIOR_HASEL = ["tommek", "mmichal", "ania"]
+ZBIOR_HASEL = ["tommek", "mmichal", "ania", "oola", "hhenio", "kaazio"]
 
 
 def losuj_haslo():
     pswrd = random.choice(ZBIOR_HASEL)
-    print("Wylosowane hasło to:", pswrd)  # do usunięcia
+    print(f'.........Wylosowane hasło to: {pswrd}...........')  # do usunięcia
     return pswrd
+
 
 def print_emtpy_pass(pswrd):
     pswrd_len = len(pswrd)
@@ -14,6 +15,7 @@ def print_emtpy_pass(pswrd):
     print(f'**** Hasło składa się z {pswrd_len} liter ****')
     print(list(empty_pswrd))
     return list(empty_pswrd)
+
 
 def find_letter(pswrd, ingame_pswrd):
     pswrd = list(pswrd)
@@ -30,21 +32,38 @@ def find_letter(pswrd, ingame_pswrd):
         print(ingame_pswrd)
         return ingame_pswrd
 
+
+def guess_full_pswrd(pswrd):
+    user_guess = input("Podaj hasło -->")
+    if user_guess == pswrd:
+        print("**** Brawo wygrałeś !!! ****")
+    else:
+        print("Podane hasło jest błędne")
+
+
 def main():
+    n = int(input("Wybierz stopień trudności (liczba prób od 3 do 10) --> "))
 
     pswrd = losuj_haslo()
 
+
     ingame_pswrd = print_emtpy_pass(pswrd)
 
-    for proba in range (1, 7):
+    for proba in range(1, n):
         print(f'***** Próba {proba} *****')
+        choice = input("Czy chcesz odgadnąć całe hasło (t/n)? -->")
+        if choice == "t":
+            guess_full_pswrd(pswrd)
+
         if ingame_pswrd == list(pswrd):
             print("Brawo wygrałeś")
             break
         else:
             find_letter(pswrd, ingame_pswrd)
 
-        if proba == 6:
+        if proba == n:
             print("Przegrałeś, spróbuj jeszcze raz")
             break
+
+
 main()
