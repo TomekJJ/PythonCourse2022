65f183c7b1157d7483fec943e547e618a660d21f
Tomek <tomek.jedynak@onet.eu>
2022-06-25 10:17:20 +0200

    first commit

---
+++ 11 Klasy/99 Przykład 1 klasy student.py
@@ -0,0 +1,43 @@
+# student_anna = {
+#   'name': 'Anna',
+#   'lastname': 'Kowalska',
+#   'age': '23',
+#   'subject': 'Computing Science',
+#   'email': 'anna.kowalska@university.com'
+# }
+#
+# student_piotr = {
+#   'name': 'Piotr',
+#   'lastname': 'Nowak',
+#   'age': '22',
+#   'subject': 'Computing Science',
+#   'email': 'piotr.nowak@university.com'
+# }
+
+class Student:
+    university_code = 'UEK'
+
+    def __init__(self, imie, nazwisko, wiek):
+       self.first = imie
+       self.last = nazwisko
+       self.age = wiek
+
+    def email(self):
+      return f'{self.first}.{self.last}@{self.university_code}.com'.lower()
+
+print('heloł')
+
+anna = Student('Anna', "Kowalska", 23)
+piotr = Student('Piotr', "Nowak", 22)
+
+print(anna.last, anna.first, anna.age, anna.email())
+print(piotr.last, piotr.first, piotr.age, piotr.email())
+
+print(anna.__dict__) # jakie pola należą do obiektu 'anna"
+print(Student.__dict__) # jakie pola nalezą do klasy student
+
+# anna wzięła ślub :)
+print('....................')
+anna.last = "Smith"
+print(anna.last, anna.first, anna.email())
+
